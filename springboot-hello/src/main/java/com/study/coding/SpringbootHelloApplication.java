fd38cffb00afe9623724b5948c7ce6d42bbaf04b
bigfish.liu <916748598@qq.com>
2019-10-01 00:40:53 +0800

    banner基础使用

---
+++ springboot-hello/src/main/java/com/study/coding/SpringbootHelloApplication.java
@@ -0,0 +1,18 @@
+package com.study.coding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 程序启动类
+ *
+ * @author liusheng
+ */
+@SpringBootApplication
+public class SpringbootHelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootHelloApplication.class, args);
+	}
+
+}
